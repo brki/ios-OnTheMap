1c313768bd0cab9aba3d52b4c5c2af67ad5c92c8
Brian King <free.as.in.speech@gmail.com>
2015-10-17 15:20:13 +0200

    wip layout location posting view

---
+++ OnTheMap/LocationPostingViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LocationPostingViewController.swift
+//  OnTheMap
+//
+//  Created by Brian on 17/10/15.
+//  Copyright © 2015 truckin'. All rights reserved.
+//
+
+import UIKit
+
+class LocationPostingViewController: UIViewController {
+	
+}
